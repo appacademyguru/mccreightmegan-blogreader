34b722e69877b6fa9362de12ecc84c5c3140a5e8
Megan McCreight <appacademy.megan@gmail.com>
2014-10-11 13:34:15 -0700

    S11

--- app/src/main/java/android/mccreightm/blogreader/BlogActivity.java
+++ app/src/main/java/android/mccreightm/blogreader/BlogActivity.java
@@ -31,5 +31,7 @@ public class BlogActivity extends Activity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
         listView.setAdapter(adapter);
+
+        new BlogPostTask().execute();
     }
 }
