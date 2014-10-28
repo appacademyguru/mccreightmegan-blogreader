97b2b50f6d7b5a9538d764e8ca49f905d3cd9d48
Megan McCreight <appacademy.megan@gmail.com>
2014-10-28 14:48:21 -0700

    I7

--- app/src/main/java/android/mccreightm/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/mccreightm/blogreader/BlogWebActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity {
     @Override
@@ -15,8 +18,16 @@ public class BlogWebActivity extends Activity {
         Intent intent = getIntent();
         Uri blogUri = intent.getData();
 
-        WebView webView = (WebView)findViewById(R.id.webView);
+        final WebView webView = (WebView)findViewById(R.id.webView);
 
         webView.loadUrl(blogUri.toString());
+
+        final ProgressBar progressBar = (ProgressBar)findViewById(R.id.webprogressBar);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
